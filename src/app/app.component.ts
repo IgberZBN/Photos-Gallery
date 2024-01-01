3f3a9150ed92713ca4e6371d51654eecdc3e773a
IgberZBN <igber_benitez@outlook.com>
2024-01-01 17:38:21 -0300

    Remove text decoration and padding from link/list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
-import { HeaderComponent } from "./shared/component/header/header.component";
+import { FooterComponent } from "@shared/components/footer/footer.component";
+import { NavComponent } from "@shared/components/nav/nav.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
